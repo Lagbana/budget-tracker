a6d3db2d7dfdb8f051ecff22e7c4c7765fb7682a
Larry_A <larryagbana@gmail.com>
2020-06-17 13:03:03 -0400

    code refactor and comments

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
+// Import dependencies
 const router = require('express').Router()
 const Transaction = require('../models/transaction.js')
 
+// Route handler that creates a new single transaction document
+// and saves the document object in the Transactions database
 router.post('/api/transaction', ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
@@ -11,6 +14,8 @@ router.post('/api/transaction', ({ body }, res) => {
     })
 })
 
+// Route handler that passes and creates an array of documents
+// and saves the array of document objects in the Transaction database
 router.post('/api/transaction/bulk', ({ body }, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
@@ -21,6 +26,9 @@ router.post('/api/transaction/bulk', ({ body }, res) => {
     })
 })
 
+// Route handler to get all transaction documents from database
+// The transactions are displayed and sorted on the home page
+// by date in descending order
 router.get('/api/transaction', (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
